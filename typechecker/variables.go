6d98c6bbbd712eedb8a08afafcb1baeeb39686f0
Fuad Hasan <fuad.cs22@gmail.com>
2024-10-21 19:57:34 +0600

    Refactor variable declaration handling in typechecker and add new shape implementations

--- typechecker/variables.go
+++ typechecker/variables.go
@@ -6,6 +6,16 @@ import (
 	"walrus/errgen"
 )
 
+// checkVariableAssignment checks the assignment of a value to a variable in the given type environment.
+// It verifies if the assignee is assignable and if the types of the assignee and the value to be assigned match.
+// If any errors are encountered during these checks, they are displayed using the error generation utility.
+//
+// Parameters:
+// - node: an AST node representing the variable assignment expression.
+// - env: a pointer to the TypeEnvironment which holds type information and the file path.
+//
+// Returns:
+// - ValueTypeInterface: the type of the value being assigned.
 func checkVariableAssignment(node ast.VarAssignmentExpr, env *TypeEnvironment) ValueTypeInterface {
 
 	Assignee := node.Assignee
@@ -15,17 +25,37 @@ func checkVariableAssignment(node ast.VarAssignmentExpr, env *TypeEnvironment) V
 		errgen.MakeError(env.filePath, Assignee.StartPos().Line, Assignee.EndPos().Line, Assignee.StartPos().Column, Assignee.EndPos().Column, err.Error()).Display()
 	}
 
-	expected := GetValueType(Assignee, env)
-	provided := GetValueType(valueToAssign, env)
+	expectedType := GetValueType(Assignee, env)
+	providedType := GetValueType(valueToAssign, env)
 
-	err := MatchTypes(expected, provided, env.filePath, valueToAssign.StartPos().Line, valueToAssign.EndPos().Line, valueToAssign.StartPos().Column, valueToAssign.EndPos().Column)
+	err := MatchTypes(expectedType, providedType, env.filePath, valueToAssign.StartPos().Line, valueToAssign.EndPos().Line, valueToAssign.StartPos().Column, valueToAssign.EndPos().Column)
 	if err != nil {
 		errgen.MakeError(env.filePath, valueToAssign.StartPos().Line, valueToAssign.EndPos().Line, valueToAssign.StartPos().Column, valueToAssign.EndPos().Column, err.Error()).Display()
 	}
 
-	return provided
+	return providedType
 }
 
+// checkVariableDeclaration checks the declaration of a variable in the given AST node
+// and updates the type environment accordingly.
+//
+// Parameters:
+// - node: The AST node representing the variable declaration statement.
+// - env: The type environment in which the variable is being declared.
+//
+// Returns:
+// - ValueTypeInterface: The type of the declared variable.
+//
+// The function performs the following steps:
+// 1. Retrieves the variable to be declared from the AST node.
+// 2. Prints a message indicating the variable being declared.
+// 3. Determines the expected type of the variable, either from an explicit type
+//    specified in the declaration or by inferring it from the assigned value.
+// 4. If both an explicit type and a value are provided, checks that the value's
+//    type matches the expected type and reports an error if they do not match.
+// 5. Declares the variable in the type environment and reports any errors that occur.
+// 6. Prints a message indicating whether the variable is a constant and its type.
+// 7. Returns a Void type indicating the end of the declaration process.
 func checkVariableDeclaration(node ast.VarDeclStmt, env *TypeEnvironment) ValueTypeInterface {
 
 	varToDecl := node.Variable
