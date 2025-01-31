11f34334a032be843dfb360509ef03d1801c8cdd
Fuad Hasan <fuad.cs22@gmail.com>
2025-01-31 16:18:16 +0600

    Add test.wal file and update main.go to use 'test' as file name; modify test.json for new variable definitions and values

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 func main() {
 
-	fileName := "better"
+	fileName := "test"
 	folder := "code"
 	filePath := fmt.Sprintf("%s/%s.wal", folder, fileName)
 	tokens := lexer.Tokenize(filePath, true)
