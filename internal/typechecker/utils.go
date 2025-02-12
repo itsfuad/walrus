88eb06f079d305c248c39ee71d9c08435ce12f47
Fuad Hasan <fuad.cs22@gmail.com>
2025-02-13 00:08:32 +0600

    docs

--- internal/typechecker/utils.go
+++ internal/typechecker/utils.go
@@ -129,6 +129,7 @@ func evalStruct(s ast.StructType, env *TypeEnvironment) Struct {
 	return typ
 }
 
+
 func evalDefaultType(defaultType ast.DataType, env *TypeEnvironment) Tc {
 	val, err := getTypeDefinition(string(defaultType.Type())) // need to get the most deep type
 	if err != nil || val == nil {
@@ -137,6 +138,16 @@ func evalDefaultType(defaultType ast.DataType, env *TypeEnvironment) Tc {
 	return val
 }
 
+// evalUD evaluates a user-defined type and returns a type-checked user-defined type.
+// It takes an analyzed user-defined type of type ast.UserDefinedType and a type environment.
+// It returns a type-checked user-defined type (Tc) with the evaluated user-defined type.
+//
+// Parameters:
+// - analyzedUD: the AST representation of the user-defined type to be evaluated.
+// - env: the type environment used for type evaluation.
+//
+// Returns:
+// - Tc: a type-checked user-defined type with the evaluated user-defined type.
 func evalUD(analyzedUD ast.UserDefinedType, env *TypeEnvironment) Tc {
 	typename := analyzedUD.AliasName
 	val, err := getTypeDefinition(typename) // need to get the most deep type
@@ -146,6 +157,16 @@ func evalUD(analyzedUD ast.UserDefinedType, env *TypeEnvironment) Tc {
 	return val
 }
 
+// evalArray evaluates an AST array type and returns a type-checked array.
+// It takes an analyzed array of type ast.ArrayType and a type environment.
+// It returns a type-checked array (Tc) with the evaluated array type.
+//
+// Parameters:
+// - analyzedArray: the AST representation of the array type to be evaluated.
+// - env: the type environment used for type evaluation.
+//
+// Returns:
+// - Tc: a type-checked array with the evaluated array type.
 func evalArray(analyzedArray ast.ArrayType, env *TypeEnvironment) Tc {
 	val := evaluateTypeName(analyzedArray.ArrayType, env)
 	arr := Array{
@@ -155,6 +176,21 @@ func evalArray(analyzedArray ast.ArrayType, env *TypeEnvironment) Tc {
 	return arr
 }
 
+// evalFn evaluates a given function type within a specified type environment and returns a type-checked function.
+//
+// Parameters:
+// - analyzedFunctionType: The AST representation of the function type to be analyzed.
+// - env: The type environment in which the function type is evaluated.
+//
+// Returns:
+// - Tc: A type-checked function containing the function's data type, parameters, return type, and function scope.
+//
+// The function performs the following steps:
+// 1. Creates a new type environment for the function scope.
+// 2. Iterates over the function parameters and checks for duplicate parameter names.
+// 3. Evaluates the type of each parameter and adds it to the list of function parameters.
+// 4. Evaluates the return type of the function.
+// 5. Constructs and returns a type-checked function with the evaluated parameters and return type.
 func evalFn(analyzedFunctionType ast.FunctionType, env *TypeEnvironment) Tc {
 
 	scope := NewTypeENV(env, FUNCTION_SCOPE, fmt.Sprintf("_FN_%s", RandStringRunes(10)), env.filePath)
@@ -186,6 +222,22 @@ func evalFn(analyzedFunctionType ast.FunctionType, env *TypeEnvironment) Tc {
 	}
 }
 
+// evalMap evaluates an AST representation of a map type and returns a type-checked
+// representation of the map. It checks if the map type is a built-in "map" type or
+// a user-defined type.
+//
+// Parameters:
+// - analyzedMap: The AST representation of the map type to be evaluated.
+// - env: The type environment containing type information and definitions.
+//
+// Returns:
+// - Tc: The type-checked representation of the map.
+//
+// If the map type is a built-in "map", it evaluates the key and value types and
+// returns a new map type. If the map type is user-defined, it retrieves the type
+// definition and checks if it is a map type. If it is, it returns a new map type
+// based on the retrieved definition. If the type is not a map, it reports an error
+// and returns a void type.
 func evalMap(analyzedMap ast.MapType, env *TypeEnvironment) Tc {
 	if analyzedMap.Map.Name == "map" {
 		keyType := evaluateTypeName(analyzedMap.KeyType, env)
@@ -208,6 +260,18 @@ func evalMap(analyzedMap ast.MapType, env *TypeEnvironment) Tc {
 	}
 }
 
+// validateTypeCompatibility checks if the provided type is compatible with the expected type.
+// It first unwraps both types and then performs a type switch on the unwrapped expected type.
+// If the expected type is an Interface, it checks if the provided type implements the required methods.
+// Otherwise, it converts both types to their string representations and compares them.
+// If the types are not compatible, it returns an error indicating the type mismatch.
+//
+// Parameters:
+//   - expectedType: The type that is expected.
+//   - providedType: The type that is provided.
+//
+// Returns:
+//   - error: An error indicating the type incompatibility, or nil if the types are compatible.
 func validateTypeCompatibility(expectedType, providedType Tc) error {
 
 	unwrappedExpected := unwrapType(expectedType)
@@ -228,6 +292,17 @@ func validateTypeCompatibility(expectedType, providedType Tc) error {
 	return nil
 }
 
+// tcToString converts a type-checker (Tc) value to its string representation.
+// It handles various types including Array, Struct, Interface, Fn, Map, Maybe,
+// and UserDefined. For each type, it returns a formatted string that represents
+// the type. If the type is nil, it returns "void". For other types, it returns
+// the string representation of the type's DType.
+//
+// Parameters:
+// - val: The type-checker (Tc) value to be converted to a string.
+//
+// Returns:
+// - A string representation of the type-checker value.
 func tcToString(val Tc) string {
 	switch t := val.(type) {
 	case Array:
@@ -252,6 +327,16 @@ func tcToString(val Tc) string {
 	}
 }
 
+// functionSignatureString generates a string representation of a function's signature.
+// It takes a function `fn` of type `Fn` as input and returns a string that describes
+// the function's parameters and return type in the format: "fn(param1: type1, param2: type2) -> returnType".
+// If the function has no return type, the return type part is omitted.
+//
+// Parameters:
+// - fn: The function whose signature is to be represented as a string.
+//
+// Returns:
+// - A string representing the function's signature.
 func functionSignatureString(fn Fn) string {
 	ParamStrs := ""
 	for i, param := range fn.Params {
@@ -269,6 +354,16 @@ func functionSignatureString(fn Fn) string {
 	return fmt.Sprintf("fn(%s)%s", ParamStrs, ReturnStr)
 }
 
+// checkMethodsImplementations checks if the provided type `src` implements the interface `dest`.
+// It returns an error if `src` does not implement `dest` or if `dest` is not an interface.
+//
+// Parameters:
+// - src: The type to check for interface implementation. It can be a struct or an interface.
+// - dest: The interface type that `src` should implement.
+//
+// Returns:
+// - error: An error if `src` does not implement `dest` or if `dest` is not an interface. 
+//          The error message includes details about the mismatch or invalid type.
 func checkMethodsImplementations(src, dest Tc) error {
 
 	//check if the provided type implements the interface
@@ -299,6 +394,14 @@ func checkMethodsImplementations(src, dest Tc) error {
 	return nil
 }
 
+// handleStructDest checks if a given struct implements all methods of a specified interface.
+// It verifies the presence, parameter types, and return types of each method.
+// If any method is missing or has a mismatch in parameters or return types, an error is appended to the errs slice.
+//
+// Parameters:
+// - src: The struct to be checked.
+// - destInterface: The interface that the struct should implement.
+// - errs: A slice to which any errors found during the check will be appended.
 func handleStructDest(src Struct, destInterface Interface, errs []error) {
 
 	// check if all methods are present
@@ -337,6 +440,20 @@ func handleStructDest(src Struct, destInterface Interface, errs []error) {
 	}
 }
 
+// handleInterfaceDest checks if all methods in the destination interface are present and compatible with the source interface.
+// It validates the parameters and return types of each method.
+//
+// Parameters:
+// - src: The source interface to be checked.
+// - destInterface: The destination interface to be checked against.
+// - errs: A slice to collect any errors found during the validation process.
+//
+// The function performs the following checks:
+// 1. Ensures that each method in the destination interface is present in the source interface.
+// 2. Validates the compatibility of method parameters between the source and destination interfaces.
+// 3. Validates the compatibility of return types between the source and destination interfaces.
+//
+// If any method is missing or has incompatible parameters or return types, an error is appended to the errs slice.
 func handleInterfaceDest(src Interface, destInterface Interface, errs []error) {
 	// both are interfaces, check if all methods are present and compatible
 	for _, interfaceMethod := range destInterface.Methods {
