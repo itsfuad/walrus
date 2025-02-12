be5f5265d3cc75420c197fbfb6f4594e30fe5c4a
Fuad Hasan <fuad.cs22@gmail.com>
2025-02-12 12:51:23 +0600

    Refactor AST and utility functions: move AST types to internal package, implement new utility functions, and update serializer logic

---
+++ internal/typechecker/utils.go
@@ -0,0 +1,332 @@
+package typechecker
+
+import (
+	//Standard packages
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	//Walrus packages
+	"walrus/internal/ast"
+	"walrus/internal/builtins"
+	"walrus/internal/report"
+	"walrus/internal/utils"
+)
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(letterRunes))
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func checkLValue(node ast.Node, env *TypeEnvironment) error {
+	//if not constant and is IdentifierExpr
+	switch t := node.(type) {
+	case ast.IdentifierExpr:
+		if isTypeDefined(t.Name) {
+			return errors.New("type")
+		}
+		//find the declaredEnv where the variable was declared
+		declaredEnv, err := env.resolveVar(t.Name)
+		if err != nil {
+			return err
+		}
+		if !declaredEnv.constants[t.Name] {
+			return nil
+		} else {
+			return errors.New("constant")
+		}
+	case ast.Indexable:
+		return checkLValue(t.Container, env)
+	case ast.StructPropertyAccessExpr:
+		return checkLValue(t.Object, env)
+	default:
+		return fmt.Errorf("invalid lvalue")
+	}
+}
+
+func isNumberType(operand Tc) bool {
+	switch operand.(type) {
+	case Int, Float:
+		return true
+	default:
+		return false
+	}
+}
+
+func isIntType(operand Tc) bool {
+	switch operand.(type) {
+	case Int:
+		return true
+	default:
+		return false
+	}
+}
+
+// evaluateTypeName evaluates the given DataType and returns a corresponding ValueTypeInterface.
+// It handles different types of DataType such as ArrayType, FunctionType, and others.
+//
+// Parameters:
+//   - dtype: The DataType to be evaluated.
+//   - env: The TypeEnvironment in which the DataType is evaluated.
+//
+// Returns:
+//   - A ValueTypeInterface representing the evaluated type.
+//
+// The function performs the following steps
+//  1. If the dtype is an ArrayType, it recursively evaluates the element type and returns an Array.
+//  2. If the dtype is a FunctionType, it evaluates the parameter types and return type, creates a new function scope, and returns a Fn.
+//  3. If the dtype is nil, it returns a Void type.
+//  4. For other types, it attempts to create a ValueTypeInterface and handles any errors that occur.
+func evaluateTypeName(dtype ast.DataType, env *TypeEnvironment) Tc {
+	switch t := dtype.(type) {
+	case ast.ArrayType:
+		return evalArray(t, env)
+	case ast.FunctionType:
+		return evalFn(t, env)
+	case ast.MapType:
+		return evalMap(t, env)
+	case ast.UserDefinedType:
+		return evalUD(t, env)
+	case ast.StructType:
+		return evalStruct(t, env)
+	case nil:
+		return NewVoid()
+	default:
+		return evalDefaultType(dtype, env)
+	}
+}
+
+func evalStruct(s ast.StructType, env *TypeEnvironment) Struct {
+
+	name := "struct { "
+	for i, prop := range s.Properties {
+
+		propType := evaluateTypeName(prop.PropType, env)
+
+		name += prop.Prop.Name + ": " + tcToString(propType)
+		if i != len(s.Properties)-1 {
+			name += ", "
+		}
+	}
+
+	name += " }"
+
+	typ := checkStructTypeDecl(name, s, env)
+
+	// declare the struct type
+	declareType(name, typ) // if the struct is already declared, it will be skipped
+
+	return typ
+}
+
+func evalDefaultType(defaultType ast.DataType, env *TypeEnvironment) Tc {
+	val, err := getTypeDefinition(string(defaultType.Type())) // need to get the most deep type
+	if err != nil || val == nil {
+		report.Add(env.filePath, defaultType.StartPos().Line, defaultType.EndPos().Line, defaultType.StartPos().Column, defaultType.EndPos().Column, err.Error()).Level(report.CRITICAL_ERROR)
+	}
+	return val
+}
+
+func evalUD(analyzedUD ast.UserDefinedType, env *TypeEnvironment) Tc {
+	typename := analyzedUD.AliasName
+	val, err := getTypeDefinition(typename) // need to get the most deep type
+	if err != nil || val == nil {
+		report.Add(env.filePath, analyzedUD.StartPos().Line, analyzedUD.EndPos().Line, analyzedUD.StartPos().Column, analyzedUD.EndPos().Column, err.Error()).Level(report.CRITICAL_ERROR)
+	}
+	return val
+}
+
+func evalArray(analyzedArray ast.ArrayType, env *TypeEnvironment) Tc {
+	val := evaluateTypeName(analyzedArray.ArrayType, env)
+	arr := Array{
+		DataType:  builtins.ARRAY,
+		ArrayType: val,
+	}
+	return arr
+}
+
+func evalFn(analyzedFunctionType ast.FunctionType, env *TypeEnvironment) Tc {
+
+	scope := NewTypeENV(env, FUNCTION_SCOPE, fmt.Sprintf("_FN_%s", RandStringRunes(10)), env.filePath)
+
+	var params []FnParam
+	for _, param := range analyzedFunctionType.Parameters {
+		//check if the parameter is already declared
+		if utils.Some(params, func(p FnParam) bool {
+			return p.Name == param.Identifier.Name
+		}) {
+			report.Add(scope.filePath, param.Identifier.Start.Line, param.Identifier.End.Line, param.Identifier.Start.Column, param.Identifier.End.Column,
+				fmt.Sprintf("parameter '%s' is already defined", param.Identifier.Name)).Level(report.CRITICAL_ERROR)
+		}
+
+		paramType := evaluateTypeName(param.Type, scope)
+		params = append(params, FnParam{
+			Name: param.Identifier.Name,
+			Type: paramType,
+		})
+	}
+
+	returns := evaluateTypeName(analyzedFunctionType.ReturnType, scope)
+
+	return Fn{
+		DataType:      builtins.FUNCTION,
+		Params:        params,
+		Returns:       returns,
+		FunctionScope: *scope,
+	}
+}
+
+func evalMap(analyzedMap ast.MapType, env *TypeEnvironment) Tc {
+	if analyzedMap.Map.Name == "map" {
+		keyType := evaluateTypeName(analyzedMap.KeyType, env)
+		valueType := evaluateTypeName(analyzedMap.ValueType, env)
+		return NewMap(keyType, valueType)
+	} else {
+		//find the name in the type definition
+		val, err := getTypeDefinition(analyzedMap.Map.Name) // need to get the most deep type
+		if err != nil {
+			report.Add(env.filePath, analyzedMap.StartPos().Line, analyzedMap.EndPos().Line, analyzedMap.StartPos().Column, analyzedMap.EndPos().Column, err.Error()).Level(report.NORMAL_ERROR)
+		}
+
+		if mapVal, ok := val.(Map); ok {
+			return NewMap(mapVal.KeyType, mapVal.ValueType)
+		}
+
+		report.Add(env.filePath, analyzedMap.StartPos().Line, analyzedMap.EndPos().Line, analyzedMap.StartPos().Column, analyzedMap.EndPos().Column, fmt.Sprintf("'%s' is not a map", analyzedMap.Map.Name)).Level(report.CRITICAL_ERROR)
+
+		return NewVoid()
+	}
+}
+
+func validateTypeCompatibility(expectedType, providedType Tc) error {
+
+	unwrappedExpected := unwrapType(expectedType)
+	unwrappedProvided := unwrapType(providedType)
+
+	switch unwrappedExpected.(type) {
+	case Interface:
+		errs := checkMethodsImplementations(unwrappedExpected, unwrappedProvided)
+		if len(errs) > 0 {
+			msgs := fmt.Sprintf("cannot use type '%s' as interface '%s'\n", tcToString(providedType), tcToString(expectedType))
+			return errors.New(msgs + report.TreeFormatError(errs...).Error())
+		}
+		return nil
+	}
+
+	expectedStr := tcToString(unwrappedExpected)
+	providedStr := tcToString(unwrappedProvided)
+
+	if expectedStr != providedStr {
+		return fmt.Errorf("cannot assign value of type '%s' to type '%s'", providedStr, expectedStr)
+	}
+
+	return nil
+}
+
+func tcToString(val Tc) string {
+	switch t := val.(type) {
+	case Array:
+		return fmt.Sprintf("[]%s", tcToString(t.ArrayType))
+	case Struct:
+		return t.StructName
+	case Interface:
+		return t.InterfaceName
+	case Fn:
+		return functionSignatureString(t)
+	case Map:
+		return fmt.Sprintf("map[%s]%s", tcToString(t.KeyType), tcToString(t.ValueType))
+	case Maybe:
+		return fmt.Sprintf("maybe{%s}", tcToString(t.MaybeType))
+	case UserDefined:
+		return tcToString(unwrapType(t.TypeDef))
+	default:
+		if t == nil {
+			return "void"
+		}
+		return string(t.DType())
+	}
+}
+
+func functionSignatureString(fn Fn) string {
+	ParamStrs := ""
+	for i, param := range fn.Params {
+		ParamStrs += param.Name
+		ParamStrs += ": "
+		ParamStrs += string(tcToString(param.Type))
+		if i != len(fn.Params)-1 {
+			ParamStrs += ", "
+		}
+	}
+	ReturnStr := string(tcToString(fn.Returns))
+	if ReturnStr != "" {
+		ReturnStr = " -> " + ReturnStr
+	}
+	return fmt.Sprintf("fn(%s)%s", ParamStrs, ReturnStr)
+}
+
+func checkMethodsImplementations(expected, provided Tc) []error {
+
+	//check if the provided type implements the interface
+	errs := []error{}
+
+	var interfaceType Interface
+	interfaceType, ok := expected.(Interface)
+	if !ok {
+		return []error{fmt.Errorf("type must be an interface")}
+	}
+
+	var structType Struct
+	structType, ok = provided.(Struct)
+	if !ok {
+		return []error{fmt.Errorf("type must be a struct")}
+	}
+
+	// check if all methods are present
+	for _, interfaceMethod := range interfaceType.Methods {
+		// check if method is present in the struct's variables
+		methodVal, ok := structType.StructScope.variables[interfaceMethod.Name]
+		if !ok {
+			errs = append(errs, fmt.Errorf("missing method '%s' on '%s'", interfaceMethod.Name, structType.StructName))
+			continue
+		}
+
+		// check if the method is a function
+		methodFn, ok := methodVal.(StructMethod)
+		if !ok {
+			//return fmt.Errorf("'%s' on struct '%s' is not a valid method for interface '%s'",
+			//	methodName, structType.StructName, interfaceType.InterfaceName)
+			errs = append(errs, fmt.Errorf("'%s' is expected to be a method", interfaceMethod.Name))
+			continue
+		}
+
+		// check the return type and parameters
+		for i, param := range interfaceMethod.Method.Params {
+			expectedParam := tcToString(param.Type)
+			providedParam := tcToString(methodFn.Fn.Params[i].Type)
+			if expectedParam != providedParam {
+				//return fmt.Errorf("method '%s' found for interface '%s' but parameter missmatch", methodName, interfaceType.InterfaceName)
+				errs = append(errs, fmt.Errorf("method '%s', but parameter missmatch", interfaceMethod.Name))
+			}
+		}
+
+		//check the return type
+		expectedReturn := tcToString(interfaceMethod.Method.Returns)
+		providedReturn := tcToString(methodFn.Fn.Returns)
+		if expectedReturn != providedReturn {
+			//return fmt.Errorf("method '%s' found for interface '%s' but return type mismatched", methodName, interfaceType.InterfaceName)
+			errs = append(errs, fmt.Errorf("method '%s' found, but return type mismatched", interfaceMethod.Name))
+		}
+	}
+
+	return errs
+}
---
+++ internal/typechecker/variables.go
@@ -0,0 +1,121 @@
+package typechecker
+
+import (
+	//Standard packages
+	"fmt"
+
+	//Walrus packages
+	"walrus/colors"
+	"walrus/internal/ast"
+	"walrus/internal/report"
+)
+
+// checkVariableAssignment checks the assignment of a value to a variable in the given type environment.
+// It verifies if the assignee is assignable and if the types of the assignee and the value to be assigned match.
+// If any errors are encountered during these checks, they are displayed using the error generation utility.
+//
+// Parameters:
+// - node: an AST node representing the variable assignment expression.
+// - env: a pointer to the TypeEnvironment which holds type information and the file path.
+//
+// Returns:
+// - ValueTypeInterface: the type of the value being assigned.
+func checkVariableAssignment(node ast.VarAssignmentExpr, env *TypeEnvironment) Tc {
+
+	Assignee := node.Assignee
+	valueToAssign := node.Value
+
+	if err := checkLValue(Assignee, env); err != nil {
+		report.Add(env.filePath, Assignee.StartPos().Line, Assignee.EndPos().Line, Assignee.StartPos().Column, Assignee.EndPos().Column, fmt.Sprintf("cannot assign to %s", err.Error())).Level(report.CRITICAL_ERROR)
+	}
+
+	expectedType := parseNodeValue(Assignee, env)
+	providedType := parseNodeValue(valueToAssign, env)
+
+	err := validateTypeCompatibility(expectedType, providedType)
+	if err != nil {
+		report.Add(env.filePath, valueToAssign.StartPos().Line, valueToAssign.EndPos().Line, valueToAssign.StartPos().Column, valueToAssign.EndPos().Column, err.Error()).Level(report.NORMAL_ERROR)
+	}
+
+	return providedType
+}
+
+// checkVariableDeclaration checks the declaration of a variable in the given AST node
+// and updates the type environment accordingly.
+//
+// Parameters:
+// - node: The AST node representing the variable declaration statement.
+// - env: The type environment in which the variable is being declared.
+//
+// Returns:
+// - ValueTypeInterface: The type of the declared variable.
+//
+// The function performs the following steps:
+//  1. Retrieves the variable to be declared from the AST node.
+//  2. Prints a message indicating the variable being declared.
+//  3. Determines the expected type of the variable, either from an explicit type
+//     specified in the declaration or by inferring it from the assigned value.
+//  4. If both an explicit type and a value are provided, checks that the value's
+//     type matches the expected type and reports an error if they do not match.
+//  5. Declares the variable in the type environment and reports any errors that occur.
+//  6. Prints a message indicating whether the variable is a constant and its type.
+//  7. Returns a Void type indicating the end of the declaration process.
+func checkVariableDeclaration(node ast.VarDeclStmt, env *TypeEnvironment) Tc {
+
+	varsToDecl := node.Variables
+
+	for _, varToDecl := range varsToDecl {
+
+		colors.BLUE.Print("Declaring variable ")
+		colors.RED.Println(varToDecl.Identifier.Name)
+
+		expectedTypeInterface := parseExpectedType(varToDecl.ExplicitType, varToDecl.Value, env)
+
+		// do not allow void type
+		if _, ok := expectedTypeInterface.(Void); ok {
+			report.Add(env.filePath, varToDecl.Identifier.StartPos().Line, varToDecl.Identifier.EndPos().Line, varToDecl.Identifier.StartPos().Column, varToDecl.Identifier.EndPos().Column, "cannot declare variable of type void\n"+report.TreeFormatString("a variable must have a non void type")).Level(report.CRITICAL_ERROR)
+		}
+
+		if varToDecl.Value != nil && varToDecl.ExplicitType != nil {
+			providedValue := parseNodeValue(varToDecl.Value, env)
+			err := validateTypeCompatibility(expectedTypeInterface, providedValue)
+			if err != nil {
+				report.Add(env.filePath, varToDecl.Value.StartPos().Line, varToDecl.Value.EndPos().Line, varToDecl.Value.StartPos().Column, varToDecl.Value.EndPos().Column, fmt.Sprintf("error declaring variable '%s'. %s", varToDecl.Identifier.Name, err.Error())).Level(report.NORMAL_ERROR)
+			}
+		}
+
+		err := env.declareVar(varToDecl.Identifier.Name, expectedTypeInterface, node.IsConst, false)
+		if err != nil {
+			report.Add(env.filePath, varToDecl.Identifier.StartPos().Line, varToDecl.Identifier.EndPos().Line, varToDecl.Identifier.StartPos().Column, varToDecl.Identifier.EndPos().Column, err.Error()).Level(report.CRITICAL_ERROR)
+		}
+
+		if node.IsConst {
+			colors.GREEN.Print("Declared constant variable ")
+			colors.RED.Print(varToDecl.Identifier.Name)
+			fmt.Print(" of type ")
+			colors.PURPLE.Println(tcToString(expectedTypeInterface))
+		} else {
+			colors.GREEN.Print("Declared variable ")
+			colors.RED.Print(varToDecl.Identifier.Name)
+			fmt.Print(" of type ")
+			colors.PURPLE.Println(tcToString(expectedTypeInterface))
+		}
+	}
+	return NewVoid()
+}
+
+func parseExpectedType(explicitType ast.DataType, value ast.Node, env *TypeEnvironment) Tc {
+	var expectedTypeInterface Tc
+
+	if explicitType != nil {
+		expectedTypeInterface = evaluateTypeName(explicitType, env)
+		fmt.Print("Explicit type: ")
+		colors.PURPLE.Println(tcToString(expectedTypeInterface))
+	} else {
+		expectedTypeInterface = parseNodeValue(value, env)
+		colors.ORANGE.Print("Auto detected type: ")
+		colors.PURPLE.Println(tcToString(expectedTypeInterface))
+	}
+
+	return expectedTypeInterface
+}
